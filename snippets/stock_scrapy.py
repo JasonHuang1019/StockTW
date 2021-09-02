6f351090200789e1cd2fd9f626a4fa0166ee93a3
Jason Huang <dreambroke1019@gmail.com>
2021-09-02 15:22:37 +0800

    first

--- snippets/stock_scrapy.py
+++ snippets/stock_scrapy.py
@@ -16,6 +16,11 @@ import time
 import pandas as pd
 import dataframe_image as dfi
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless") #無頭模式
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 def total():
     today=datetime.date.today()
